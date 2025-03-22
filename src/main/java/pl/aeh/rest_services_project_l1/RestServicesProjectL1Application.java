642b0a7763100e008e225f6a8ad5303791e08939
JBryskiewicz <kuba@fiku.eu>
2025-03-22 11:17:51 +0100

    init

---
+++ src/main/java/pl/aeh/rest_services_project_l1/RestServicesProjectL1Application.java
@@ -0,0 +1,13 @@
+package pl.aeh.rest_services_project_l1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesProjectL1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServicesProjectL1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/aeh/rest_services_project_l1/RestServicesProjectL1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aeh.rest_services_project_l1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicesProjectL1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
